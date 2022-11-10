f066fdee2607e1e9b8cfc24e7c1ef1d61ba5f8e7
sayankarmakar <sayank@capitalnumbers.com>
2022-11-10 23:17:03 +0530

    code re-structure

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -2,19 +2,26 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+        <div class="row">
+            <div class="col-sm-6">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in as Customer!') }}
+                        <h5 class="card-title">Update profile</h5>
+                        <p class="card-text">Edit your basic details like name, email etc</p>
+                        <a href="{{ route('admin.create-user') }}">
+                            <button type="button" class="btn btn-dark">Edit Profile</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Display products</h5>
+                        <p class="card-text">Click here to view listed products on our site</p>
+                        <a href="{{ route('admin.view-users') }}">
+                            <button type="button" class="btn btn-dark">View Products</button>
+                        </a>
                     </div>
                 </div>
             </div>
