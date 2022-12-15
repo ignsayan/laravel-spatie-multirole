18ba8e906e52477c9e874ff4ff63de11a74eb4bb
sayankarmakar <sayank@capitalnumbers.com>
2022-12-15 13:10:30 +0530

    code refactor

--- resources/vue/settings/Permissions.vue
+++ resources/vue/settings/Permissions.vue
@@ -1,51 +1,51 @@
 <template>
-  <div class="row">
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Assigned Permissions</h5>
-          <div
-            class="form-check form-switch"
-            v-for="permission in activePermissions"
-            :key="permission.id"
-          >
-            <input
-              class="form-check-input"
-              type="checkbox"
-              v-bind:value="permission.name"
-              name="permission[]"
-              checked
-            />
-            <label class="form-check-label">{{ permission.name }}-user</label>
-          </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Assigned Permissions</h5>
+                    <div
+                        v-for="permission in activePermissions"
+                        :key="permission.id"
+                        class="form-check form-switch"
+                    >
+                        <input
+                            checked
+                            class="form-check-input"
+                            name="permission[]"
+                            type="checkbox"
+                            v-bind:value="permission.name"
+                        />
+                        <label class="form-check-label">{{ permission.name }}-user</label>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="col-sm-6">
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Available Permissions</h5>
-          <div
-            class="form-check form-switch"
-            v-for="permission in inactivePermissions"
-            :key="permission.id"
-          >
-            <input
-              class="form-check-input"
-              type="checkbox"
-              v-bind:value="permission.name"
-              name="permission[]"
-            />
-            <label class="form-check-label">{{ permission.name }}-user</label>
-          </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Available Permissions</h5>
+                    <div
+                        v-for="permission in inactivePermissions"
+                        :key="permission.id"
+                        class="form-check form-switch"
+                    >
+                        <input
+                            class="form-check-input"
+                            name="permission[]"
+                            type="checkbox"
+                            v-bind:value="permission.name"
+                        />
+                        <label class="form-check-label">{{ permission.name }}-user</label>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  props: ["activePermissions", "inactivePermissions"],
+    props: ["activePermissions", "inactivePermissions"],
 };
 </script>
